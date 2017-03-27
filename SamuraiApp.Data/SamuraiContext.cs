74507cd6426210d526cd2f068f96d4ab7c060207
Vincent Gim <vgim@sateva.com.au>
2017-03-27 22:24:41 +0800

    Added EF migration

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        // EF will call this internally.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // We pulled in Microsoft.EntityFrameworkCore.SqlServer
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=SamuraiData; Trusted_Connection=True;");
+        }
+    }
+}
