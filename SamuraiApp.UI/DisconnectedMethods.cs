0b3a37e1d9122451504cf0a68dff2a183270093e
Vincent Gim <vgim@sateva.com.au>
2017-04-18 23:06:04 +0800

    Added display state function

---
+++ SamuraiApp.UI/DisconnectedMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace SamuraiApp.UI
+{
+    public class DisconnectedMethods
+    {
+        private static void DisplayState(List<EntityEntry> es, string method)
+        {
+            es.ForEach(e => Console.WriteLine(
+                $"{e.Entity.GetType().Name} : {e.State.ToString()}"));
+            Console.WriteLine();
+        }
+    }
+}
